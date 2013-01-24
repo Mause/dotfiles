125d4ecba3f95600df7282828c77c3e851d28d4a
Dominic May <jack.thatch@gmail.com>
2013-01-24 22:43:02 +0800

    Made prompt git status more concise, fixed make_symlinks.sh script

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,21 +3,23 @@ function cls
 end
 
 set fish_git_dirty_color red
+set fish_git_clean_color green
 
-function parse_git_dirty
-    git diff --quiet HEAD ^&-
-    if test $status = 1
-        echo -n (set_color $fish_git_dirty_color)"Δ"(set_color normal)
-    end
-end
-
-function parse_git_branch
-     echo -n (parse_git_dirty) (git branch | sed -rn 's/\* (\w+)/\1/p')
+function parse_git_status
+     git diff --quiet HEAD ^&-
+     if test $status = 1
+          set_color $fish_git_dirty_color
+     else
+          set_color $fish_git_clean_color
+     end
+     echo -n (git branch | sed -rn 's/\* (\w+)/\1/p')
+     set_color normal
 end
 
 
 function git_folder_status
      if test -d .git
-          echo -n "["(parse_git_branch)"]"
+          echo -n "["(parse_git_status)"]"
      end
 end
+
