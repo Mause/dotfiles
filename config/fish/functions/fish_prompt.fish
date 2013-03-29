b076a576739567a1134236c8e213c51e08faca83
Dominic May <jack.thatch@gmail.com>
2013-03-29 19:26:35 +0800

    Derp.

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -7,21 +7,27 @@ function fish_prompt
     end
     echo -n "•"
     set_color normal
+
     echo -n "<"
     set_color blue
     echo -n (whoami)
+
     set_color yellow
     echo -n "@"
+
     set_color red
     echo -n (hostname|cut -d . -f 1)
+
     set_color yellow
     echo -n ":"
+
     set_color $fish_color_cwd
     if test (prompt_pwd);
         echo -n (prompt_pwd);
-    else;
+    else
         echo -n "/";
-    end;
+    end
+
     set_color normal
     echo -n (git_folder_status)
     echo -n '> '
