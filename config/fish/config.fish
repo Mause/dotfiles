7d375c064dad76d698ece264c02dd86a17ba74c4
Dominic May <me@mause.me>
2019-01-12 21:25:46 +0800

    Switch to builtin git support in fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,29 +1,4 @@
-set fish_git_dirty_color red
-set fish_git_clean_color green
-set fish_greeting ""
-
-function cls
-    clear
-end
-
 function sshn
     sudo shutdown -h now
-end
-
-function parse_git_status
-     git diff --quiet HEAD ^&-
-     if test $status = 1
-          set_color $fish_git_dirty_color
-     else
-          set_color $fish_git_clean_color
-     end
-     echo -n (git branch | sed -rn 's/\* (\w+)/\1/p')
-     set_color normal
-end
-
-function git_folder_status
-     if test -d .git
-          echo -n "["(parse_git_status)"]"
-     end
 end
 
--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,3 +1,20 @@
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
 
 function fish_prompt
     # display the status of the last run command
@@ -36,6 +53,6 @@ function fish_prompt
 
     # git branch
     set_color normal
-    echo -n (git_folder_status)
+    printf '%s ' (__fish_git_prompt)
     echo -n '> '
 end
