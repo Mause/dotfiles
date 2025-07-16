9f95126a92d1914c673b366274bdc594895ad0ec
Elliana May <me@mause.me>
2025-07-16 11:55:12 +0800

    add trouble to renovate

--- compile_renovate.py
+++ compile_renovate.py
@@ -1,25 +1,40 @@
 import json
 import re
 from pprint import pprint
+from pathlib import Path
 
-with open('renovate.json') as fh:
+with open("renovate.json") as fh:
     renovate = json.load(fh)
 
-with open('config/nvim/lua/plugins/treesitter.lua') as fh:
-    lua = fh.read()
 
-deps = re.findall(r''''([^']+)'|"([^"]+)"''', lua)
-deps = [(a or b) for a, b, in deps]
-deps = [a.split('/') for a in deps if '/' in a]
-pprint(deps)
+def get_deps(filename):
+    with filename.open() as fh:
+        lua = fh.read()
 
-template = ''.join([("{{# if (equals depName '%s') }}%s{{/if}}" % (depname, owner)) for owner, depname in deps])
+    deps = re.findall(r''''([^']+)'|"([^"]+)"''', lua)
+    deps = [(a or b) for a, b in deps]
+    deps = [a.split("/") for a in deps if "/" in a and ' ' not in a]
+    pprint(deps)
+    return deps
+
+
+deps = [
+    dep
+    for filename in Path("config/nvim/lua/plugins").glob("*.lua")
+    for dep in get_deps(filename)
+]
+
+template = "".join(
+    [
+        ("{{# if (equals depName '%s') }}%s{{/if}}" % (depname, owner))
+        for owner, depname in deps
+    ]
+)
 print(template)
 
-res = f'https://github.com/{template}/{{{{depName}}}}'
+res = f"https://github.com/{template}/{{{{depName}}}}"
 print(res)
-renovate['customManagers'][0]['depNameTemplate'] = res
+renovate["customManagers"][0]["depNameTemplate"] = res
 
-with open('renovate.json', 'w') as fh:
+with open("renovate.json", "w") as fh:
     json.dump(renovate, fh, indent=2)
-
