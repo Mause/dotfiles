654837df5a96eec5a8c9e1dcabbb440b7c51e457
Elliana May <emay@atlassian.com>
2025-07-16 14:40:25 +0800

    parse lua

--- compile_renovate.py
+++ compile_renovate.py
@@ -1,7 +1,12 @@
+# /// script
+# dependencies = ['slpp', 'pdbpp']
+# ///
+
 import json
 import re
 from pprint import pprint
 from pathlib import Path
+from slpp import slpp
 
 with open("renovate.json") as fh:
     renovate = json.load(fh)
@@ -11,11 +16,22 @@ def get_deps(filename):
     with filename.open() as fh:
         lua = fh.read()
 
-    deps = re.findall(r''''([^']+)'|"([^"]+)"''', lua)
-    deps = [(a or b) for a, b in deps]
-    deps = [a.split("/") for a in deps if "/" in a and ' ' not in a]
-    pprint(deps)
-    return deps
+    contents = slpp.decode(lua[len('return'):])
+
+    for dep in (contents.values() if isinstance(contents, dict) else contents):
+        res = {}
+
+        if isinstance(dep, dict):
+            for k, v in dep.items():
+                if k == 0:
+                    res['name'] = v
+                elif k == 'branch':
+                    res['branch'] = v
+        else:
+            res['name'] = dep[0]
+
+        yield res
+
 
 
 deps = [
@@ -27,8 +43,8 @@ deps.append(("folke", "lazy.nvim.git"))
 
 template = "".join(
     [
-        ("{{# if (equals depName '%s') }}%s{{/if}}" % (depname, owner))
-        for owner, depname in deps
+        "{{# if (equals depName '%s') }}%s{{/if}}" % tuple(dep['name'].split('/'))
+        for dep in deps
     ]
 )
 print(template)
