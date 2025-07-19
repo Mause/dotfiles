47d034ce7a27586c6669d8a27a56ae7195d613fc
Elliana May <me@mause.me>
2025-07-19 18:17:18 +0800

    parametrize branch

--- config/fish/functions/merge-upstream.fish
+++ config/fish/functions/merge-upstream.fish
@@ -1,11 +1,12 @@
-function merge-upstream
+function merge-upstream --argument-names branch
 	set url (basename (pwd))
 	set url "/repos/Mause/$url/merge-upstream"
 	echo $url
+    set -q branch[1]; or set branch main
 	gh api \
 		--method POST \
 		-H "Accept: application/vnd.github+json" \
 		-H "X-GitHub-Api-Version: 2022-11-28" \
 		$url \
-		-f branch='main'
+        -f branch=$branch
 end
