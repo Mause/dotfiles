33b2cd1e89236a58debd5b699000da9185412ebb
Elliana May <me@mause.me>
2025-08-03 20:34:26 +0800

    force enable termguicolors

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,6 +2,9 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
 require("config.lazy")
 require("config.mason")
 require("config.lspconfig")
