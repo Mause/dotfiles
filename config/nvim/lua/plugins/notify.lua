3985bfac5f3e927a58b2ba59b21e8d015cb86f13
Elliana May <me@mause.me>
2025-08-07 20:46:14 +0800

    add notify plugin

---
+++ config/nvim/lua/plugins/notify.lua
@@ -0,0 +1,18 @@
+return {
+  ---@type LazySpec
+  {
+    'rcarriga/nvim-notify',
+    init = function()
+      --      require('notify').setup({
+      --        background_colour = '#000000',
+      --        stages = 'fade_in_slide_out',
+      --        timeout = 3000,
+      --        max_height = function()
+      --          return math.floor(vim.o.lines * 0.75)
+      --        end,
+      --      })
+      vim.notify = require('notify')
+    end,
+  },
+  'pynappo/git-notify.nvim',
+}
