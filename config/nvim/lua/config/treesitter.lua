ba852486b6ee4fe6c6b96fed45b762dc81b67a77
Elliana May <me@mause.me>
2025-08-09 19:24:24 +0800

    include doc treesitter parsers

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -1,6 +1,14 @@
 -- Treesitter Plugin Setup
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "rust", "toml", "lua" },
+  ensure_installed = {
+    "rust",
+    "toml",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "vim",
+    "vimdoc"
+  },
   auto_install = true,
   highlight = {
     enable = true,
