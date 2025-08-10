b528f239d3fae971225872c1fd9ea5544506e2c0
Elliana May <me@mause.me>
2025-08-10 15:23:51 +0800

    add standalone Snacks decl

---
+++ config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+  ---@type LazySpec
+  {
+    "folke/snacks.nvim",
+    lazy = false,
+    priority = 1000,
+    keys = {
+      { "<leader>lg", "<CMD>:lua Snacks.lazygit()<CR>", { desc = "[L]azy[G]it" } }
+    },
+    ---@type snacks.Config
+    opts = {
+      lazygit = {}
+    }
+  }
+}
