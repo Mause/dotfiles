d5de458472c0967a88645d0e1ca2623a9b0acddb
Elliana May <me@mause.me>
2025-08-26 10:44:56 +0800

    add source column to blink

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -1,7 +1,29 @@
-return { {
-  "Saghen/blink.cmp",
-  version = "1.*",
-  opts = {
-    keymap = { preset = "default" },
+return {
+  {
+    "Saghen/blink.cmp",
+    version = "1.*",
+    opts = {
+      keymap = { preset = "default" },
+      completion = {
+        menu = {
+          draw = {
+            columns = {
+              { "kind_icon" },
+              { "label", "label_description", "source_name", gap = 1 },
+            },
+            components = {
+              source_name = {
+                text = function(ctx)
+                  if ctx.source_id == "cmdline" then
+                    return
+                  end
+                  return ctx.source_name --:sub(1, 4)
+                end,
+              },
+            },
+          },
+        },
+      },
+    },
   },
-} }
+}
