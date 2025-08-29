4ebde705619a340f42236fac50fc2ee2fc1b47a6
Elliana May <emay@atlassian.com>
2025-08-29 17:54:34 +0800

    enable snacks terminal

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -6,11 +6,13 @@ return {
     priority = 1000,
     keys = {
       { "<leader>lg", "<CMD>:lua Snacks.lazygit()<CR>", { desc = "[L]azy[G]it" } },
+      { "<leader>tm", "<CMD>:lua Snacks.terminal()<CR>", { desc = "[T]er[M]inal" } },
     },
     ---@type snacks.Config
     opts = {
       lazygit = {},
       input = { enabled = true },
+      terminal = {},
     },
   },
 }
