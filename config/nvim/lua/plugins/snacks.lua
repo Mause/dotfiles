e29542e858235d8aac8876cc58da45faf0f45690
Elliana May <emay@atlassian.com>
2025-09-01 10:05:10 +0800

    rework dashboard

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -16,22 +16,46 @@ return {
       dashboard = {
         sections = {
           {
-            section = "header",
+            pane = 1,
+            { section = "header" },
+            { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+            { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+            { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+            { section = "startup" },
+            function()
+              ---@type snacks.dashboard.Text
+              return {
+                text = {
+                  { "Neovim version: ", hl = "footer" },
+                  { tostring(vim.version()), hl = "special" },
+                },
+                align = "center",
+              }
+            end,
           },
-          { section = "keys", padding = 2 },
           {
-            section = "startup",
+            pane = 2,
+            {
+              section = "terminal",
+              cmd = "chafa ~/Downloads/R013_C039_F01@2x.png --format symbols --symbols vhalf --size 60x17 --stretch", --; sleep .1",
+              height = 17,
+              padding = 1,
+              ttl = 5 * 60,
+            },
+            {
+              icon = " ",
+              title = "Git Status",
+              section = "terminal",
+              enabled = function()
+                return Snacks.git.get_root() ~= nil
+              end,
+              cmd = "git status --short --branch --renames",
+              height = 5,
+              padding = 1,
+              ttl = 5 * 60,
+              indent = 3,
+            },
           },
-          function()
-            ---@type snacks.dashboard.Text
-            return {
-              text = {
-                { "Neovim version: ", hl = "footer" },
-                { tostring(vim.version()), hl = "special" },
-              },
-              align = "center",
-            }
-          end,
         },
       },
     },
