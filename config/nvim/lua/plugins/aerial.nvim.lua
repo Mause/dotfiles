a1aefbe77d59166b547bd2fd269d88f73f8a9713
Elliana May <me@mause.me>
2025-09-02 21:19:50 +0800

    bind aerial

--- config/nvim/lua/plugins/aerial.nvim.lua
+++ config/nvim/lua/plugins/aerial.nvim.lua
@@ -1,9 +1,13 @@
 -- Plugin: stevearc/aerial.nvim
 -- Installed via store.nvim
 
+---@type LazySpec
 return {
   "stevearc/aerial.nvim",
   opts = {},
+  keys = {
+    { "<leader>la", "<cmd>AerialToggle!<CR>", desc = "Toggle Aerial" },
+  },
   -- Optional dependencies
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
