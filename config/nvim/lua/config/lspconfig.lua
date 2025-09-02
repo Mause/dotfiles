f0d43ae17fd4e6dbddfca985212b0e01927a9957
Elliana May <me@mause.me>
2025-09-02 21:53:40 +0800

    enable format on write

--- config/nvim/lua/config/lspconfig.lua
+++ config/nvim/lua/config/lspconfig.lua
@@ -64,6 +64,20 @@ vim.api.nvim_create_autocmd("LspAttach", {
       end
     end
 
+    local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+    if
+      not client:supports_method("textDocument/willSaveWaitUntil")
+      and client:supports_method("textDocument/formatting")
+    then
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
+        buffer = ev.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
+        end,
+      })
+    end
+
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
