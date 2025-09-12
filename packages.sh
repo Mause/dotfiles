765e15121a18603bcf929b282826de997019ecc1
Elliana May <me@mause.me>
2025-09-12 17:10:23 +1000

    add packages list

---
+++ packages.sh
@@ -0,0 +1,16 @@
+pkg i neovim git
+pkg i sccache
+pkg i ripgrep
+pkg i gitoxide
+pkg i fd bat
+pkg i ruff
+pkg i libxml2 libxslt
+pkg i diff-so-fancy
+pkg i uv
+pkg i gh direnv chafa
+pkg i gh direnv
+pkg i gh direnc
+pkg i file
+pkg i which
+pkg i tmux
+pkg i fish
