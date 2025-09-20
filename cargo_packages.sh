2dcc6a45243eb3e8b7cd64996d215fc845f8efaf
Elliana May <me@mause.me>
2025-09-20 19:29:19 +0800

    dotenv-linter

--- cargo_packages.sh
+++ cargo_packages.sh
@@ -1,4 +1,5 @@
 cargo install git-absorb --locked
+cargo install dotenv-linter --locked
 cargo install --git https://github.com/tombi-toml/tombi tombi-cli --locked
 cargo install harper-ls --locked
 cargo install emmylua_ls --locked
