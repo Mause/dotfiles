4e172a7bde31078cefa25a6b524e948b719048de
Elliana May <emay@atlassian.com>
2025-10-31 15:27:25 +0800

    replace header

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -17,7 +17,17 @@ return {
         sections = {
           {
             pane = 1,
-            { section = "header" },
+            {
+              section = "terminal",
+              cmd = "fortune -s | cowsay",
+              hl = "header",
+              padding = 1,
+              height = 20,
+              indent = 8,
+            },
+          },
+          {
+            pane = 2,
             { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
             { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
             { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
