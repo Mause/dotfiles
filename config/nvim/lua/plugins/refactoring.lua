89a654e5dfe490e5cbfe4aeb074c65ead70ad6a2
Elliana May <me@mause.me>
2025-11-02 21:45:17 +0800

    more bindings

--- config/nvim/lua/plugins/refactoring.lua
+++ config/nvim/lua/plugins/refactoring.lua
@@ -21,6 +21,29 @@ return {
         mode = "v",
         desc = "Select refactor",
       },
+      -- You can also use below = true here to to change the position of the printf
+      -- statement (or set two remaps for either one). This remap must be made in normal mode.
+      {
+        "<leader>rp",
+        function() require('refactoring').debug.printf({ below = false }) end
+        ,
+        mode = "n",
+      },
+
+      -- Print var
+      {
+        "<leader>rv",
+        function() require('refactoring').debug.print_var() end,
+        -- Supports both visual and normal mode
+        mode = { "x", "n" },
+      },
+
+      {
+        "<leader>rc",
+        function() require('refactoring').debug.cleanup({}) end,
+        -- Supports only normal mode
+        mode = "n",
+      }
     },
   },
 }
