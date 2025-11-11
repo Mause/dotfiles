cce4c221e1fd635106c9c4f62e365ad94569c962
Elliana May <me@mause.me>
2025-11-11 15:26:26 +0800

    add blink.indent

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -1,5 +1,21 @@
 return {
   {
+    'saghen/blink.pairs',
+    version = '*', -- (recommended) only required with prebuilt binaries
+    -- opts = {
+    --   highlights = {
+    --     enabled = true,
+    --   }
+    -- },
+  },
+  -- lazy.nvim
+  {
+    'saghen/blink.indent',
+    --- @module 'blink.indent'
+    --- @type blink.indent.Config
+    -- opts = {},
+  },
+  {
     "Saghen/blink.cmp",
     version = "1.*",
     opts = {
@@ -9,7 +25,7 @@ return {
           draw = {
             columns = {
               { "kind_icon" },
-              { "label", "label_description", "source_name", gap = 1 },
+              { "label",    "label_description", "source_name", gap = 1 },
             },
             components = {
               source_name = {
