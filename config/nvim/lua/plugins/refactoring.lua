e7a9a1e3d5690006514286a090771c184c7e53da
Elliana May <me@mause.me>
2026-01-04 14:58:23 +0800

    Enable prompting for return and parameter types in refactoring.nvim

--- config/nvim/lua/plugins/refactoring.lua
+++ config/nvim/lua/plugins/refactoring.lua
@@ -9,6 +9,20 @@ return {
     config = function()
       require("refactoring").setup({
         show_success_message = true,
+        -- prompt for return type
+        prompt_func_return_type = {
+            go = true,
+            cpp = true,
+            c = true,
+            java = true,
+        },
+        -- prompt for function parameters
+        prompt_func_param_type = {
+            go = true,
+            cpp = true,
+            c = true,
+            java = true,
+        },
       })
       require("telescope").load_extension("refactoring")
     end,
