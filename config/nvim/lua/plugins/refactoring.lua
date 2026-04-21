1389e5c13597231c4528cf39cf70e69ed959bb0b
Elliana May <me@mause.me>
2026-04-21 22:59:47 +0800

    remove dead config

--- config/nvim/lua/plugins/refactoring.lua
+++ config/nvim/lua/plugins/refactoring.lua
@@ -8,21 +8,6 @@ return {
     lazy = false,
     config = function()
       require("refactoring").setup({
-        show_success_message = true,
-        -- prompt for return type
-        prompt_func_return_type = {
-            go = true,
-            cpp = true,
-            c = true,
-            java = true,
-        },
-        -- prompt for function parameters
-        prompt_func_param_type = {
-            go = true,
-            cpp = true,
-            c = true,
-            java = true,
-        },
       })
     end,
     keys = {
