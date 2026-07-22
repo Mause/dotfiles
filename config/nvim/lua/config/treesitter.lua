d5fbd8b9b72faf10d807286916b74aeb02e82944
Elliana May <me@mause.me>
2026-07-22 10:06:21 +0800

    cut over to tree-sitter main

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -1,25 +1,34 @@
 -- Treesitter Plugin Setup
-require("nvim-treesitter.configs").setup({
-  ensure_installed = {
-    "rust",
-    "toml",
+require("nvim-treesitter").install(
+  {
+    "diff",
+    "gitcommit",
     "lua",
     "markdown",
     "markdown_inline",
+    "python",
+    "rust",
+    "toml",
     "vim",
     "vimdoc",
-    "diff",
-    "gitcommit",
-  },
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  ident = { enable = true },
-  rainbow = {
-    enable = true,
-    extended_mode = true,
-    max_file_lines = nil,
-  },
+  })
+
+vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '<filetype>' },
+  callback = function() vim.treesitter.start() end,
 })
+
+--   auto_install = true,
+--   highlight = {
+--     enable = true,
+--     additional_vim_regex_highlighting = false,
+--   },
+--   ident = { enable = true },
+--   rainbow = {
+--     enable = true,
+--     extended_mode = true,
+--     max_file_lines = nil,
+--   },
+-- })
